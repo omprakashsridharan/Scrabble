ce56916d83a1221a09aad26a4b5a4285c68b08d6
Sridharan <osridharan@lm-maa-22004973.paypalcorp.com>
2018-08-06 14:09:36 +0530

    Read from file

---
+++ src/Scrabble.java
@@ -0,0 +1,20 @@
+public class Scrabble {
+
+    private String[] rack;
+
+    public Scrabble(String[] rack) {
+        this.rack = rack;
+    }
+
+    public String[] getRack() {
+        return rack;
+    }
+
+    public void setRack(String[] rack) {
+        this.rack = rack;
+    }
+
+    public static void getMaxWordAtStart(){
+
+    }
+}
